05097ff56038068dd2a49b72eaa08b6b3400a4d9
jsl0149 <jsl0149@naver.com>
2024-10-17 05:50:29 +0900

    refactor : Skill 리스트 리펙토링

--- frontend/src/app/components/Skill/Skill.tsx
+++ frontend/src/app/components/Skill/Skill.tsx
@@ -2,10 +2,17 @@
 
 import useStore from '../AboutMe/Store/store';
 import Image from 'next/image';
-import { STACK_ICON, STACK_BACK, STACK_DEV, STACK_FRONT } from './Images/image';
+import {
+  STACK_ICON,
+  STACK_BACK,
+  STACK_DEV,
+  STACK_FRONT,
+  STACK_TOOL,
+} from './Images/image';
 import frontend from '../../images/front.svg';
 import backend from '../../images/backend.svg';
 import devops from '../../images/devops.svg';
+import tool from '../../images/tool.svg';
 
 const Skill = () => {
   const { isVisible } = useStore();
@@ -20,17 +27,9 @@ const Skill = () => {
           isVisible ? 'opacity-100 animate-fade_in_slow ' : 'opacity-0'
         }`}
       >
-        <div className="relative w-full flex flex-row top-[10px]">
-          <h3 className="text-[2rem] font-poppins text-center">Skills</h3>
+        <div className="relative w-fit flex flex-row top-[10px]">
+          <h1 className="text-[7.5rem] font-poppins">Skills</h1>
         </div>
-        <div className="relative w-full h-fit ">
-          <div
-            className={`absolute bg-white h-[0.5px] w-0 left-0 ${
-              isVisible && 'animate-liner'
-            }`}
-          ></div>
-        </div>
-
         <Detail></Detail>
       </div>
     </section>
@@ -39,57 +38,71 @@ const Skill = () => {
 
 const Detail = () => {
   return (
-    <div className="flex flex-col gap-[4rem] w-full">
-      <div className="flex flex-col gap-3 w-full">
-        <div className="text-[1rem] flex flex-row gap-3 mb-3">
-          <Image
-            src={frontend.src}
-            alt="frontend"
-            width={50}
-            height={50}
-          ></Image>
-          <span className="relative w-fit font-poppins text-[2rem]">
-            <div className="relative z-10">Frontend Dev</div>
-            <div className="absolute w-full h-2 bg-blue-500 bottom-2 z-0"></div>
-          </span>
-        </div>
-        <div className="font-poppins font-bold text-[1rem]">
-          가장 많이 사용해 봤어요.
-        </div>
-        {STACK_FRONT.map((item) => {
-          return <StackItem stackItem={item} key={item.name}></StackItem>;
-        })}
+    <div className="relative w-full flex flex-row justify-between">
+      <div className="flex flex-col gap-[4rem] w-full">
+        <StackList
+          title="Frontend Dev"
+          detail="자주 다뤄봤어요."
+          stackItemArray={STACK_FRONT}
+          color="blue"
+          icon={backend.src}
+        ></StackList>
+        <StackList
+          title="Backend Dev"
+          detail="사용해 본 경험이 있어요."
+          stackItemArray={STACK_BACK}
+          color="pink"
+          icon={backend.src}
+        ></StackList>
       </div>
-      <div className="flex flex-col gap-3 w-full">
-        <div className="text-[1rem] flex flex-row gap-3 mb-3">
-          <Image src={backend.src} alt="backend" width={50} height={50}></Image>
-          <span className="relative w-fit font-poppins text-[2rem]">
-            <div className="relative z-10">Backend Dev</div>
-            <div className="absolute w-full h-2 bg-[#EA06D3] bottom-2 z-0"></div>
-          </span>
-        </div>
-        <div className="font-poppins font-bold text-[1rem]">
-          사용해 본 경험이 있어요.
-        </div>
-        {STACK_BACK.map((item) => {
-          return <StackItem stackItem={item} key={item.name}></StackItem>;
-        })}
+      <div className="flex flex-col gap-[4rem] w-full">
+        <StackList
+          title="DevOps"
+          detail="Github로 프로젝트 관리를 할 수 있어요."
+          stackItemArray={STACK_DEV}
+          color="orange"
+          icon={devops.src}
+        ></StackList>
+        <StackList
+          title="Tools"
+          detail="현재 사용하고 있는 Tool 이에요."
+          stackItemArray={STACK_TOOL}
+          color="orange"
+          icon={tool.src}
+        ></StackList>
       </div>
-      <div className="flex flex-col gap-3 w-full">
-        <div className="text-[1rem] flex flex-row gap-3 mb-3">
-          <Image src={devops.src} alt="devops" width={50} height={50}></Image>
-          <span className="relative w-fit font-poppins text-[2rem]">
-            <div className="relative z-10">DevOps & Tools</div>
-            <div className="absolute w-full h-2 bg-[#DA7227] bottom-2 z-0"></div>
-          </span>
-        </div>
-        <div className="font-poppins font-bold text-[1rem]">
-          Github로 프로젝트 관리를 할 수 있어요.
-        </div>
-        {STACK_DEV.map((item) => {
-          return <StackItem stackItem={item} key={item.name}></StackItem>;
-        })}
+    </div>
+  );
+};
+
+interface StackListProps {
+  title: string;
+  stackItemArray: StackItemType[];
+  detail: string;
+  color: string;
+  icon: string;
+}
+
+const StackList: React.FC<StackListProps> = (props) => {
+  const color: { [key: string]: string } = {
+    orange: 'absolute w-full h-2 bg-[#DA7227] bottom-2 z-0',
+    pink: 'absolute w-full h-2 bg-[#EA06D3] bottom-2 z-0',
+    blue: 'absolute w-full h-2 bg-blue-500 bottom-2 z-0',
+  };
+
+  return (
+    <div className="flex flex-col gap-3 w-full">
+      <div className="text-[1rem] flex flex-row gap-3 mb-3">
+        <Image src={props.icon} alt="devops" width={50} height={50}></Image>
+        <span className="relative w-fit font-poppins text-[2rem]">
+          <div className="relative z-10">{props.title}</div>
+          <div className={color[props.color]}></div>
+        </span>
       </div>
+      <div className="font-poppins font-bold text-[1rem]">{props.detail}</div>
+      {props.stackItemArray.map((item) => {
+        return <StackItem stackItem={item} key={item.name}></StackItem>;
+      })}
     </div>
   );
 };
